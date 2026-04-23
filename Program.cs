7a6b682442b65cbea216d111ff68119daeaec2ff
Amir01122009 <samir01122009@yandex.ru>
2026-04-23 18:19:05 +0300

    Добавлена первая программа

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace first_Git___app
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет, Git-сервис!");
+            Console.WriteLine($"Дата: {DateTime.Now:dd.MM.yyyy}");
+        }
+    }
+}
